807123cc95c919ce2f6afe3a6647c67981a34c73
jfields-ctrl <jfields@bbprepcs.com>
2025-12-16 12:26:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
-// Array of image paths
+// Array of all your images
 const images = [
-  'images/5ae60826d132.jpg', // first image
-  'images/2df571c30279.jpg', // second image
-  'images/b9173b7b3fe9.jpg' // third image
+  'images/0463479f0e85.jpg',
+  'images/172a13fc765d.jpg',
+  'images/23fc9ff40911.jpg'
 ];
 
 let currentIndex = 0;
 const slide = document.getElementById('slide');
 
 function nextSlide() {
-  currentIndex = (currentIndex + 1) % images.length;
+  currentIndex = (currentIndex + 1) % images.length; // loop back
   slide.src = images[currentIndex];
 }
 
 // Change image every 3 seconds
 setInterval(nextSlide, 3000);
-
-
